3d15ed27b4f7355a900efa2826e63920b2b6354f
budarajuraman <ramanrahul114@gmail.com>
2024-07-18 14:16:30 +0530

    blog_app

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BookIcon from '@mui/icons-material/Book';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+      <BookIcon fontSize="large" />
+      BlogMaster</h1>
+    </header>
+  );
+}
+
+export default Header;
